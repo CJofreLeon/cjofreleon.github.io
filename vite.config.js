a36cfb93c4aa8de3eba528db922b6bb1eac7df7e
Catalina <catalina.jofre.l@usach.cl>
2025-08-18 12:44:58 -0400

    update: vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/", // reemplaza por el nombre del repo
+  base: "/portafolio", 
   plugins: [react()],
 });
